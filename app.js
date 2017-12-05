cc9b09675e9faa2c6c673daac0e0134d2a9e6746
Trung <huutrung.mmt@gmail.com>
2017-12-05 13:19:46 +0700

    update

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let bodyParser = require('body-parser');
 const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
 const assert = require('assert');
+let request = require('request');
 
 let app = express();
 let fs = require("fs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**DANH SACH CAC DOANH NGHIEP TRONG BUSINESS DB*/
 app.get('/listBusiness', function (req, res) {
     MongoClient.connect('mongodb://admin:uitvn@ds127436.mlab.com:27436/lab1_business?authMechanism=SCRAM-SHA-1', function (err, db) {
         assert.equal(null, err);
@@ -20,6 +22,16 @@ app.get('/listBusiness', function (req, res) {
     });
 });
 
+/**THONG TIN KHACH HANG NAO DA SU DUNG SAN PHAM CUA TUNG DOANH NGHIEP*/
+app.get('/customer-detail-from-client', function (req, res) {
+    request('http://localhost:8082/customerDetail', function(error, response, data) {
+        if (!error && response.statusCode == 200) {
+            res.json(data);
+        }
+    });
+});
+
+/**THONG TIN CHI TIET DOANH NGHIEP*/
 app.get('/businessDetail/:id', function (req, res) {
     MongoClient.connect('mongodb://admin:uitvn@ds127436.mlab.com:27436/lab1_business?authMechanism=SCRAM-SHA-1', function (err, db) {
         assert.equal(null, err);
@@ -32,9 +44,9 @@ app.get('/businessDetail/:id', function (req, res) {
             res.json(false);
         }
     });
-
 });
 
+/**UPDATE SAN PHAM(producID) THUOC DOANH NGHIEP(businessID) NAO DUOC KHACH HANG(customerID) MUA VOI SO LUONG BAO NHIEU*/
 app.post('/update/product/status', function (req, res) {
     MongoClient.connect('mongodb://admin:uitvn@ds127436.mlab.com:27436/lab1_business?authMechanism=SCRAM-SHA-1', function (err, db) {
         assert.equal(null, err);
--- app_client.js
+++ app_client.js
@@ -13,6 +13,7 @@ const dbClient1 = 'mongodb://client1:uitvn@ds127536.mlab.com:27536/lab1_client1'
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**GOI API CAP NHAT BEN PHIA SERVER TRA VE DANH SACH DOANH NGHIEP*/
 app.get('/list-business-from-server', function (req, res) {
     request('http://localhost:8081/listBusiness', function(error, response, data) {
     	if (!error && response.statusCode == 200) {
@@ -20,6 +21,53 @@ app.get('/list-business-from-server', function (req, res) {
   		}
     });
 });
+
+/**CHI TIET KHACH HANG MUA SAN PHAM CUA CONG TY NAO*/
+app.get('/customerDetail', function (req, res) {
+    try{
+        MongoClient.connect(dbClient1, async function(err, db) {
+            if (err) throw err;
+            const col = db.collection('product_selling');
+            await col.aggregate([
+                // Join with user_info table
+                {
+                    $lookup:{
+                        from: "customer",       // other table name
+                        localField: "customerID",   // name of product_selling table field
+                        foreignField: "customerID", // name of customer table field
+                        as: "customer_info"         // alias for customer table
+                    }
+                },
+                // Join with product table
+                {
+                    $lookup:{
+                        from: "product",
+                        localField: "businessID",
+                        foreignField: "businessID",
+                        as: "product_info"
+                    }
+                },
+                {   $unwind:"$customer_info" },
+                {   $unwind:"$product_info" },
+                {
+                    $project:{
+                        _id : 1,
+                        quantity: 1,
+                        name : "$customer_info.name",
+                        address: "$customer_info.address",
+                        product_bought : "$product_info.name",
+                        product_price: "$product_info.price"
+                    }
+                }
+            ],function (err,result) {
+                res.json(result);
+            });
+        });
+    }catch (e){
+        res.json(e);
+    }
+});
+
 app.get('/list-business/:productID', function (req, res) {
     try {
         MongoClient.connect(dbClient1, async function (err, db) {
@@ -29,7 +77,6 @@ app.get('/list-business/:productID', function (req, res) {
             await db.collection('product').find({"productID": req.params.productID}).toArray().then( function (products) {
                 db.close();
                 list_product = products;
-
             });
             if (list_product) {
                 for (let i = 0; i < list_product.length; i++) {
